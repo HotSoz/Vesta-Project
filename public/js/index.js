fe95bfc1d4c7b1da7a8cb4aeabdc2c276329ebf2
Raymond Nhim <raymondnh@function-HotSoz.localdomain>
2021-02-04 13:27:26 -0800

    fixing the like and dislike button

--- public/js/index.js
+++ public/js/index.js
@@ -61,92 +61,137 @@ $(document).ready(
       $(this).css("height", scrlHeight + "px");
       // oninput='this.style.height = ""; this.style.height = this.scrollHeight + "px"'
     });
+
+    //New Like and Dislike
     // Buttons for Like and Dislike
-    let litLike = $(".checkLike");
-    let ditLike = $(".checkDislike");
-    let disPoint = $(".dislike");
-    let dislikes = $(".dislikeCount").text();
-    let dislikeNum = parseInt(dislikes);
-    let likePoint = $(".like");
-    let likes = $(".likeCount").text();
-    let likeNum = parseInt(likes);
-    // Like Button
-    $(".like").click(function () {
+    // let litLike = $(".checkLike");
+    // let ditLike = $(".checkDislike");
+    // let disPoint = $(".dislike");
+    // let dislikes = $(".dislikeCount").text();
+    // let dislikeNum = parseInt(dislikes);
+    // let likePoint = $(".like");
+    // // let likes = $(".likeCount").text();
+    // // let likeNum = parseInt(likes);
+    // // Like Button
+    // $(".like").click(function () {
 
-      if (ditLike.hasClass("checked" && "disliked")) {
-        ditLike.removeClass("checked");
-        litLike.addClass("checked");
-        $(".dislike").removeClass("disliked");
-        $(".dislike").addClass("undisliked");
-        dislikeNum--;
-        $(".dislikeCount").html(dislikeNum);
-      } else {
-        litLike.addClass("checked");
-      }
+    //   let likeElement = $(this).children().last().html();
+    //   console.log(likeElement);
+    //   let likes = (likeElement);
+    //   let likeNum = parseInt(likes)
 
-      if ($(likePoint).hasClass("liked")) {
-        likeNum--;
-        ditLike.removeClass("checked");
-        $(".like").removeClass("liked");
-        $(".like").addClass("unliked");
-        $(".likeCount").html(likeNum);
+    //   if (ditLike.hasClass("checked" && "disliked")) {
+    //     ditLike.removeClass("checked");
+    //     litLike.addClass("checked");
+    //     $(".dislike").removeClass("disliked");
+    //     $(".dislike").addClass("undisliked");
+    //     dislikeNum--;
+    //     $(".dislikeCount").html(dislikeNum);
+    //   } else {
+    //     litLike.addClass("checked");
+    //   }
+
+    //   if ($(likePoint).hasClass("liked")) {
+    //     likeNum--;
+    //     ditLike.removeClass("checked");
+    //     $(".like").removeClass("liked");
+    //     $(".like").addClass("unliked");
+    //     $(".likeCount").html(likeNum);
+    //     return;
+    //   }
+
+    //   if ($(likePoint).hasClass("unliked")) {
+    //     likeNum++;
+    //     litLike.addClass("checked");
+    //     $(".like").removeClass("unliked");
+    //     $(".like").addClass("liked");
+    //   }
+
+    //   $('.likeCount').html(likeNum);
+    // });
+
+    // // Dislike Button
+    // $(".dislike").click(function () {
+      
+      
+
+    //   if ($(litLike).hasClass("checked" && "liked")) {
+    //     $(".like").removeClass("liked");
+    //     $(".like").addClass("unliked");
+    //     likeNum--;
+    //     $(".likeCount").html(likeNum);
+    //   } else {
+    //     ditLike.addClass("checked");
+    //   }
+      
+
+    //   if ($(disPoint).hasClass("disliked")) {
+    //     dislikeNum--;
+    //     litLike.removeClass("checked");
+    //     $(".dislike").removeClass("disliked");
+    //     $(".dislike").addClass("undisliked");
+    //     $(".dislikeCount").html(dislikeNum);
+    //     return;
+    //   }
+
+    //   if ($(disPoint).hasClass("undisliked")) {
+    //     dislikeNum++;
+    //     ditLike.addClass("checked");
+    //     $(".dislike").removeClass("undisliked");
+    //     $(".dislike").addClass("disliked");
+    //   }
+    //   $(".dislikeCount").html(dislikeNum);
+    // });
+    $('.like').click(function () {
+
+      let likeNum = $(this).children().last().html();
+      let thisPoint = $(this)
+      let likePoint = parseInt(likeNum);
+    
+      if ($(this).hasClass('unliked')) {
+        $(thisPoint).removeClass('unliked');
+        $(thisPoint).addClass('liked');
+        likePoint++;
+        $(likeNum).html(likePoint);
+        console.log(likeNum)
         return;
       }
-
-      if ($(likePoint).hasClass("unliked")) {
-        likeNum++;
-        litLike.addClass("checked");
-        $(".like").removeClass("unliked");
-        $(".like").addClass("liked");
+    
+      if ($(this).hasClass('liked')) {
+        $(thisPoint).removeClass('liked');
+        $(thisPoint).addClass('unliked');
+        likePoint--;
       }
 
-      $(".likeCount").html(likeNum);
-    });
-
-    // Dislike Button
-    $(".dislike").click(function () {
-      
-      
-
-      if ($(litLike).hasClass("checked" && "liked")) {
-        $(".like").removeClass("liked");
-        $(".like").addClass("unliked");
-        likeNum--;
-        $(".likeCount").html(likeNum);
-      } else {
-        ditLike.addClass("checked");
-      }
-      
-
-      if ($(disPoint).hasClass("disliked")) {
-        dislikeNum--;
-        litLike.removeClass("checked");
-        $(".dislike").removeClass("disliked");
-        $(".dislike").addClass("undisliked");
-        $(".dislikeCount").html(dislikeNum);
-        return;
-      }
-
-      if ($(disPoint).hasClass("undisliked")) {
-        dislikeNum++;
-        ditLike.addClass("checked");
-        $(".dislike").removeClass("undisliked");
-        $(".dislike").addClass("disliked");
-      }
-      $(".dislikeCount").html(dislikeNum);
+      $(likeNum).html(likePoint);
     });
   }
 );
 
 // Comment section
 function updateCom() {
-  let y = "<div class='likeButton'><i class='like unliked checkLike far fa-heart'></i><p class='likeCount'>0</p><i class='dislike undisliked checkDislike fas fa-heart-broken'></i><p class='dislikeCount'>0</p></div>"
+  let y = 
+  `
+  <div class="likeButton">
+  <div class="like unliked checkLike">
+      <i class='far fa-heart'></i>
+      <p class="likeCount">0</p>
+  </div>
+  <div class="dislike undisliked checkDislike">
+      <i class="fas fa-heart-broken"></i>
+      <p class="dislikeCount">0</p>
+  </div>
+</div>
+`
   var x = $("#commentTxt").val();
   //$('#comments').first().clone().appendTo('#commentDiv').html();
   $("#commentDiv").append(
-    "<div id='comments'><img alt='PFP' src='Images/m2.png'><div class='gridby22'><p>Name N.</p><p class='commentPar'>" +
-      x +
-      "</p>" + y  );
+    `
+    <div id='comments'>
+    <img alt='PFP' src='Images/m2.png'>
+    <div class='gridby22'><p>Name N.</p>
+    <p class='commentPar'>` + x +
+    `</p>` + y);
   $("#commentTxt").val("");
   $(".txtcom").css("height", "");
 }
